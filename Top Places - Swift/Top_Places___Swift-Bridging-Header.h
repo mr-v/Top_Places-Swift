24efa3037c20c6106cf51811fac44cb3c54292c8
Witold <witold.skibniewski@gmail.com>
2014-11-24 19:53:56 +0100

    add cocoapods with nocilla

---
+++ Top Places - Swift/Top_Places___Swift-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Top_Places___Swift-Bridging-Header.h
+//  Top Places - Swift
+//
+//  Created by Witold Skibniewski on 24/11/14.
+//  Copyright (c) 2014 Witold Skibniewski. All rights reserved.
+//
+
+#ifndef Top_Places___Swift_Top_Places___Swift_Bridging_Header_h
+#define Top_Places___Swift_Top_Places___Swift_Bridging_Header_h
+
+import "Nocilla.h"
+
+#endif
